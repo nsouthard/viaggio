407e5b2ff1fe7ecebfd90918fa9831b9aa078bf1
nsouthard <nathan.southard@gmail.com>
2017-06-25 23:05:45 -0500

    end of night save

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,16 +6,22 @@ class LocationsController < ApplicationController
 
 
   def new
-
+    @location = Location.new
   end
 
 
   def create
+        user = current_user.id
         location = Location.new(
-                            min: params[:min],
-                            max: params[:max]
+                            name: params[:name],
+                            long: params[:long],
+                            lat: params[:lat]
                             )
         location.save
+        if location.save
+          location_preference = LocationPreference.create!(user_id: current_user.id, location_id: location.id)
+          redirect_to "/location_preferences/"
+        end
     end
 
 
@@ -25,19 +31,20 @@ class LocationsController < ApplicationController
 
 
     def edit
-      @location = Location.find_by(params[:user_id])
+      @location = Location.find_by(params[:location_id])
     end
 
     def update
-      location = Location.find_by(params[:user_id])
+      location = Location.find_by(params[:location_id])
       location.assign_attributes(
+                            name: params[:name],
                             min: params[:min],
                             max: params[:max]
                             )
     
       location.save
       flash[:success] = "Location Updated"
-      redirect_to "/users/#{user.id}"
+      redirect_to "/location_preferences/"
     end
 
 
